8a4fbe306aa5659a6bd6fe95baaa966b391e8b0e
Moritz Palm <moritz.palm@web.de>
2023-03-09 18:31:24 +0100

    added database query to get the summoner name

--- leaguify_web/views.py
+++ leaguify_web/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SummonerName
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+def summonerName(request, SummonerName_id):
+    all_names = SummonerName.objects.all()
+    name = all_names[SummonerName_id]
+    return HttpResponse("Your summoner name is %s" % name)
