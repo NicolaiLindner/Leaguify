a923a8e324144db8b05ad4786ca98028a1268cae
Nicolai Lindner <nicolai.emanuel.lindner@gmail.com>
2023-04-02 12:59:06 +0200

    Added most player champions with mastery points and corresponding champion picture

--- pages/project1.tsx
+++ pages/project1.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Grid,
   Spacer,
@@ -8,6 +8,21 @@ import {
   Card,
   Text,
 } from "@nextui-org/react";
+
+type ChampionMastery = {
+  championId: number;
+  championPoints: number;
+};
+
+type ChampionData = {
+  key: string;
+  name: string;
+  id: string;
+};
+
+type ChampionsData = {
+  [championName: string]: ChampionData;
+};
 
 export default function Project1() {
   const [summonerName, setSummonerName] = useState("");
@@ -20,6 +35,53 @@ export default function Project1() {
   const [summonerLosses, setSummonerLosses] = useState("");
   const [summonerWinrate, setSummonerWinrate] = useState("");
   const [summonerIconUrl, setSummonerIconUrl] = useState("");
+  const [championMasteries, setChampionMasteries] = useState<ChampionMastery[]>(
+    []
+  );
+
+  // Add getChampionInfo function here
+  const getChampionInfo = (championId: number) => {
+    if (!championsData) return { name: "", imageUrl: "" };
+
+    for (const championName in championsData) {
+      if (championsData[championName].key === championId.toString()) {
+        const imageUrl = `https://ddragon.leagueoflegends.com/cdn/11.22.1/img/champion/${championsData[championName].id}.png`;
+        return { name: championName, imageUrl };
+      }
+    }
+
+    return { name: "", imageUrl: "" };
+  };
+
+  const fetchChampionsData = async () => {
+    const response = await fetch(
+      "https://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json"
+    );
+    const data = await response.json();
+    return data.data;
+  };
+
+  const [championsData, setChampionsData] = useState<ChampionsData | null>(
+    null
+  );
+
+  useEffect(() => {
+    fetchChampionsData().then((data) => setChampionsData(data));
+  }, []);
+
+  const getChampionName = (championId: number) => {
+    if (!championsData) return "";
+
+    for (const championName in championsData) {
+      if (championsData[championName].key === championId.toString()) {
+        return championName;
+      }
+    }
+
+    return "";
+  };
+
+  //Getting the picture of the champion
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -39,6 +101,7 @@ export default function Project1() {
       setSummonerLosses(data.summonerLosses);
       setSummonerWinrate(data.summonerWinrate);
       setSummonerIconUrl(data.summonerIconUrl);
+      setChampionMasteries(data.championMasteries);
     } catch (error) {
       console.error(error);
     }
@@ -83,6 +146,33 @@ export default function Project1() {
               <Text>Summoner wins: {summonerWins}</Text>
               <Text>Summoner losses: {summonerLosses}</Text>
               <Text>Summoner winrate: {summonerWinrate}</Text>
+            </div>
+          </Card>
+        </div>
+      )}
+      {championMasteries.length > 0 && (
+        <div className="champion-masteries">
+          <Card>
+            <div className="champion-masteries-info">
+              <Text h4>Most Played Champions</Text>
+              {championMasteries.slice(0, 5).map((mastery, index) => {
+                // Use getChampionInfo function to get champion information (name and imageUrl)
+                const championInfo = getChampionInfo(mastery.championId);
+                return (
+                  <div key={index} className="champion-mastery">
+                    <Image
+                      src={championInfo.imageUrl}
+                      alt={`${championInfo.name} profile icon`}
+                      width={40}
+                      height={40}
+                    />
+                    <Text>
+                      {index + 1}. {championInfo.name}:{" "}
+                      {mastery.championPoints.toLocaleString()} Points
+                    </Text>
+                  </div>
+                );
+              })}
             </div>
           </Card>
         </div>
