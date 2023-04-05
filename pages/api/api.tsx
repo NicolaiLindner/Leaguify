16af76e7ed20c788b8ec3d06704acb590fb165fe
Nicolai Lindner <nicolai.emanuel.lindner@gmail.com>
2023-04-05 23:46:51 +0200

    Added a main API fetcher component

--- pages/api/api.tsx
+++ pages/api/api.tsx
@@ -1,313 +1,76 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-// Interfaces for the different data structures used in the script
-interface SummonerInfo {
+const RIOT_API_KEY = process.env.REACT_RIOT_API_KEY;
+
+export interface SummonerData {
+  id: string;
+  accountId: string;
+  puuid: string;
   name: string;
-  level: number;
-  icon: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
 }
 
-interface RankedInfo {
-  rank: string;
+export interface RankedData {
   tier: string;
-  lp: number;
+  rank: string;
+  leaguePoints: number;
   wins: number;
   losses: number;
-  winrate: number;
+  winRate: number;
+  imageUrl: string;
 }
 
-interface ChampionMasteryInfo {
-  championName: string;
-  championLevel: number;
-  championPoints: number;
-}
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { summonerName, region } = req.query;
 
-interface ItemInfo {
-  [itemId: string]: {
-    name: string;
-    icon: string;
-    stats: {
-      [statName: string]: number;
-    };
-  };
-}
-
-interface RuneMasteryInfo {
-  runes: {
-    id: number;
-    key: string;
-    icon: string;
-    name: string;
-    shortDesc: string;
-    longDesc: string;
-  }[];
-  masteries: {
-    id: number;
-    key: string;
-    icon: string;
-    name: string;
-    ranks: {
-      [rank: number]: {
-        effect: string;
-      };
-    };
-  }[];
-}
-
-interface MatchDetails {
-  championName: string;
-  kda: string;
-  items: {
-    name: string;
-    icon: string;
-  }[];
-  summonerSpells: {
-    name: string;
-    icon: string;
-  }[];
-  runes: {
-    name: string;
-    icon: string;
-  }[];
-  masteries: {
-    name: string;
-    ranks: {
-      [rank: number]: {
-        effect: string;
-      };
-    };
-  }[];
-}
-
-interface MatchSummary {
-  championName: string;
-  kda: string;
-  items: string[];
-  summonerSpells: string[];
-  runes: string[];
-  masteries: string[];
-}
-
-interface SummonerData {
-  summonerInfo: SummonerInfo;
-  rankedInfo: RankedInfo;
-  championMasteryInfo: ChampionMasteryInfo[];
-  itemInfo: ItemInfo;
-  runeMasteryInfo: RuneMasteryInfo;
-  matchHistory: string[];
-  matchSummaries: MatchSummary[];
-}
-
-// Function to get basic Summoner information (name, level, icon) based on a summoner name
-async function getSummonerInfo(summonerName: string): Promise<SummonerInfo> {
-  const { data } = await axios.get(
-    `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(
-      summonerName
-    )}?api_key=${process.env.RIOT_API_KEY}`
-  );
-  // Fetch the data from Riot's API
-  return {
-    name: data.name,
-    level: data.summonerLevel,
-    icon: `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/profileicon/${data.profileIconId}.png`,
-  };
-}
-// Function to get ranked information (rank, tier, LP, wins, losses, winrate) based on an encrypted Summoner ID, fetch data from Riot's API and extract relevant ranked information
-async function getRankedInfo(encryptedSummonerId: string): Promise<RankedInfo> {
-  const { data } = await axios.get(
-    `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encodeURIComponent(
-      encryptedSummonerId
-    )}?api_key=${process.env.RIOT_API_KEY}`
-  );
-
-  const soloRanked = data.find(
-    (entry) => entry.queueType === "RANKED_SOLO_5x5"
-  );
-
-  return {
-    rank: soloRanked?.tier || "Unranked",
-    tier: soloRanked?.rank || "",
-    lp: soloRanked?.leaguePoints || 0,
-    wins: soloRanked?.wins || 0,
-    losses: soloRanked?.losses || 0,
-    winrate:
-      (soloRanked?.wins / (soloRanked?.wins + soloRanked?.losses)) * 100 || 0,
-  };
-}
-// Function to get champion mastery information (champion name, level, points) based on an encrypted Summoner ID
-async function getChampionMasteryInfo(
-  encryptedSummonerId: string
-): Promise<ChampionMasteryInfo[]> {
-  const { data } = await axios.get(
-    `https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${encodeURIComponent(
-      encryptedSummonerId
-    )}?api_key=${process.env.RIOT_API_KEY}`
-  );
-
-  return data.map((championMastery) => ({
-    championName: championMastery.championName,
-    championLevel: championMastery.championLevel,
-    championPoints: championMastery.championPoints,
-  }));
-}
-
-async function getItemInfo(): Promise<ItemInfo> {
-  const { data } = await axios.get(
-    `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/data/en_US/item.json`
-  );
-
-  const itemInfo: ItemInfo = {};
-
-  for (const itemId in data.data) {
-    const item = data.data[itemId];
-
-    itemInfo[itemId] = {
-      name: item.name,
-      icon: `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/item/${itemId}.png`,
-      stats: item.stats,
-    };
+  if (!summonerName || typeof summonerName !== "string") {
+    res.status(400).json({ error: "Invalid summoner name" });
+    return;
   }
 
-  return itemInfo;
-}
-
-async function getRuneMasteryInfo(): Promise<RuneMasteryInfo> {
-  const { data } = await axios.get(
-    `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/data/en_US/runesReforged.json`
-  );
-
-  const runeMasteryInfo: RuneMasteryInfo = {
-    runes: [],
-    masteries: [],
-  };
-
-  for (const rune of data) {
-    runeMasteryInfo.runes.push({
-      id: rune.id,
-      key: rune.key,
-      icon: `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/${rune.icon}`,
-      name: rune.name,
-      shortDesc: rune.shortDesc,
-      longDesc: rune.longDesc,
-    });
-
-    for (const mastery of rune.slots[0].runes) {
-      runeMasteryInfo.masteries.push({
-        id: mastery.id,
-        key: mastery.key,
-        icon: `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/${mastery.icon}`,
-        name: mastery.name,
-        ranks: mastery.ranks,
-      });
-    }
+  if (!region || typeof region !== "string") {
+    res.status(400).json({ error: "Invalid region" });
+    return;
   }
 
-  return runeMasteryInfo;
-}
+  try {
+    const summonerResponse = await axios.get<SummonerData>(
+      `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`,
+      {
+        headers: {
+          "X-Riot-Token": RIOT_API_KEY,
+        },
+      }
+    );
 
-async function getMatchHistory(puuid: string): Promise<string[]> {
-  const { data } = await axios.get(
-    `https://euw1.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=${process.env.RIOT_API_KEY}`
-  );
+    const rankedResponse = await axios.get(
+      `https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerResponse.data.id}`,
+      {
+        headers: {
+          "X-Riot-Token": RIOT_API_KEY,
+        },
+      }
+    );
 
-  return data;
-}
+    const rankedData: RankedData[] = rankedResponse.data.map((entry: any) => ({
+      tier: entry.tier,
+      rank: entry.rank,
+      leaguePoints: entry.leaguePoints,
+      wins: entry.wins,
+      losses: entry.losses,
+      winRate: (entry.wins / (entry.wins + entry.losses)) * 100,
+      imageUrl: `https://opgg-static.akamaized.net/images/medals/${entry.tier.toLowerCase()}_${
+        entry.rank
+      }.png?image=q_auto:best&v=1`,
+    }));
 
-async function getMatchDetails(matchId: string): Promise<MatchDetails> {
-  const { data } = await axios.get(
-    `https://euw1.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${process.env.RIOT_API_KEY}`
-  );
+    res.status(200).json({ summonerData: summonerResponse.data, rankedData });
+  } catch (error: any) {
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+};
 
-  const participant = data.info.participants.find(
-    (participant) => participant.puuid === data.metadata.participants[0]
-  );
-  const summonerSpells = [
-    `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/spell/${participant.summoner1Id}.png`,
-    `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/spell/${participant.summoner2Id}.png`,
-  ];
-  const matchDetails: MatchDetails = {
-    championName: participant.championName,
-    kda: `${participant.kills}/${participant.deaths}/${participant.assists}`,
-    items: [
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/item/${participant.item0}.png`,
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/item/${participant.item1}.png`,
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/item/${participant.item2}.png`,
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/item/${participant.item3}.png`,
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/item/${participant.item4}.png`,
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/item/${participant.item5}.png`,
-    ],
-    summonerSpells: [
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/spell/${participant.summoner1Id}.png`,
-      `http://ddragon.leagueoflegends.com/cdn/${process.env.LATEST_DDRAGON_VERSION}/img/spell/${participant.summoner2Id}.png`,
-    ],
-    runes: participant.perks.styles.flatMap((style) => [
-      `http://ddragon.leagueoflegends.com/cdn/img/${style.icon}`,
-      ...style.selections.flatMap((selection) =>
-        selection.runes.map(
-          (rune) => `http://ddragon.leagueoflegends.com/cdn/img/${rune.icon}`
-        )
-      ),
-    ]),
-    masteries: participant.perks.styles.flatMap((style) =>
-      style.selections.flatMap((selection) =>
-        Object.values(selection.perkIds).flatMap((perkId) => {
-          const perk = participant.perks.perkIds[perkId];
-          return [
-            {
-              name: perk.name,
-              ranks: perk.ranks.reduce(
-                (obj, rank) => ({
-                  ...obj,
-                  [rank]: { effect: perk.effectValues[rank - 1] },
-                }),
-                {}
-              ),
-            },
-          ];
-        })
-      )
-    ),
-  };
-
-  return matchDetails;
-}
-
-async function getSummonerData(summonerName: string): Promise<SummonerData> {
-  const { data: summoner } = await axios.get(
-    `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(
-      summonerName
-    )}?api_key=${process.env.RIOT_API_KEY}`
-  );
-
-  const [
-    summonerInfo,
-    rankedInfo,
-    championMasteryInfo,
-    itemInfo,
-    runeMasteryInfo,
-    matchHistory,
-  ] = await Promise.all([
-    getSummonerInfo(summonerName),
-    getRankedInfo(summoner.id),
-    getChampionMasteryInfo(summoner.id),
-    getItemInfo(),
-    getRuneMasteryInfo(),
-    getMatchHistory(summoner.puuid),
-  ]);
-
-  const matchDetails = await Promise.all(
-    matchHistory.map((matchId) => getMatchDetails(matchId))
-  );
-
-  const summonerData: SummonerData = {
-    ...summonerInfo,
-    rankedInfo,
-    championMasteryInfo,
-    itemInfo,
-    runeMasteryInfo,
-    matchDetails,
-  };
-
-  return summonerData;
-}
+export default handler;
