748c2a0b9b14a4a26e11188cf25c94757739f42d
Nicolai Lindner <nicolai.emanuel.lindner@gmail.com>
2023-04-05 23:46:12 +0200

    Updated the theme

--- components/NavbarComponent.tsx
+++ components/NavbarComponent.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Button, Text } from "@nextui-org/react";
+import { Navbar, Button, Text, Image, Spacer } from "@nextui-org/react";
 import Link from "next/link";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
@@ -16,19 +16,19 @@ const NavbarComponent = () => {
   return (
     <Navbar isBordered isCompact>
       <Navbar.Brand as={Link} href="/">
-        Leaguify
+        <Text style={{ color: "#fff" }}>Leaguify</Text>
       </Navbar.Brand>
       <Navbar.Content hideIn="xs" variant="highlight-rounded">
-        <Navbar.Link href="/project1"> Project 1 </Navbar.Link>
-        <Navbar.Link href="/project2"> Project 2 </Navbar.Link>
-        <Navbar.Link href="/chat-analysis"> Project 3 </Navbar.Link>
+        <Navbar.Link href="/project1">Project 1</Navbar.Link>
+        <Navbar.Link href="/project2">Project 2</Navbar.Link>
+        <Navbar.Link href="/project3">Project 3</Navbar.Link>
       </Navbar.Content>
-
+      <Spacer />
       <Navbar.Content>
         {!user ? (
           <>
             <Navbar.Link href="/login">
-              <Button auto flat>
+              <Button auto flat color="primary">
                 Login
               </Button>
             </Navbar.Link>
@@ -36,10 +36,10 @@ const NavbarComponent = () => {
         ) : (
           <>
             <Navbar.Item>
-              <Text>Hey, {user?.email}</Text>
+              <Text style={{ color: "#fff" }}>Hey, {user?.email}</Text>
             </Navbar.Item>
             <Navbar.Item>
-              <Button auto flat onPress={() => signOutUser()}>
+              <Button auto flat color="primary" onPress={() => signOutUser()}>
                 Sign Out
               </Button>
             </Navbar.Item>
@@ -49,5 +49,4 @@ const NavbarComponent = () => {
     </Navbar>
   );
 };
-
 export default NavbarComponent;
