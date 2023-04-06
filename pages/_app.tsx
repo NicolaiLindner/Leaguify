7286cd35b4345a18b80a36a6dc8f1321985a2424
Moritz Palm <moritz.palm@web.de>
2023-04-06 14:36:19 +0200

    removed footer and box

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { NextUIProvider, createTheme } from "@nextui-org/react";
-import { Box } from "../components/Box";
 import Navbar from "../components/NavbarComponent";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
-import Footer from "@/components/FooterComponent";
 import theme from "../styles/theme";
 
 //Navbar
@@ -19,20 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <SessionContextProvider supabaseClient={supabaseClient}>
       <NextUIProvider theme={theme}>
         <Navbar />
-        <Box
-          css={{
-            px: "$12",
-            py: "$15",
-            mt: "$12",
-            "@xsMax": { px: "$10" },
-            maxWidth: "800px",
-            margin: "0 auto",
-            minHeight: "calc(100vh - 60px)",
-          }}
-        >
           <Component {...pageProps} />
-        </Box>
-        <Footer />
       </NextUIProvider>
     </SessionContextProvider>
   );
