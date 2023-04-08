b78fff0f394c997292f1495e152137d430cef803
Nicolai Lindner <nicolai.emanuel.lindner@gmail.com>
2023-04-08 15:10:39 +0200

    Displaying match IDs

--- pages/project1.tsx
+++ pages/project1.tsx
@@ -1,234 +1,87 @@
-import { useState, useEffect } from "react";
-import {
-  Grid,
-  Spacer,
-  Input,
-  Button,
-  Image,
-  Card,
-  Text,
-  Row,
-  Col,
-  Table,
-} from "@nextui-org/react";
+import React, { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import MatchHistory from "./api/matchhistory";
 
-type ChampionMastery = {
-  championId: number;
-  championPoints: number;
-};
+const Project1: React.FC = () => {
+  const [level, setLevel] = useState<number | null>(null);
+  const [puuid, setPuuid] = useState<string | null>(null);
+  const [matchIds, setMatchIds] = useState<
+    { matchId: string; matchDetails: any }[] | null
+  >(null);
 
-type ChampionData = {
-  key: string;
-  name: string;
-  id: string;
-};
-
-type ChampionsData = {
-  [championName: string]: ChampionData;
-};
-
-export default function Project1() {
-  const [summonerName, setSummonerName] = useState("");
-  const [summonerLevel, setSummonerLevel] = useState(0);
-  const [summonerId, setSummonerId] = useState("");
-  const [summonerRank, setSummonerRank] = useState("");
-  const [summonerTier, setSummonerTier] = useState("");
-  const [summonerLp, setSummonerLp] = useState("");
-  const [summonerWins, setSummonerWins] = useState("");
-  const [summonerLosses, setSummonerLosses] = useState("");
-  const [summonerWinrate, setSummonerWinrate] = useState("");
-  const [summonerIconUrl, setSummonerIconUrl] = useState("");
-  const [championMasteries, setChampionMasteries] = useState<ChampionMastery[]>(
-    []
-  );
-
-  // Add getChampionInfo function here
-  const getChampionInfo = (championId: number) => {
-    if (!championsData) return { name: "", imageUrl: "" };
-
-    for (const championName in championsData) {
-      if (championsData[championName].key === championId.toString()) {
-        const imageUrl = `https://ddragon.leagueoflegends.com/cdn/11.22.1/img/champion/${championsData[championName].id}.png`;
-        return { name: championName, imageUrl };
-      }
+  const getMatchRegion = (region: string) => {
+    switch (region) {
+      case "euw1":
+        return "europe";
+      case "na1":
+        return "americas";
+      case "eun1":
+        return "europe";
+      // Add more regions if needed
+      default:
+        return region;
     }
-
-    return { name: "", imageUrl: "" };
   };
 
-  const fetchChampionsData = async () => {
-    const response = await fetch(
-      "https://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json"
-    );
-    const data = await response.json();
-    return data.data;
-  };
-
-  const [championsData, setChampionsData] = useState<ChampionsData | null>(
-    null
-  );
-
-  useEffect(() => {
-    fetchChampionsData().then((data) => setChampionsData(data));
-  }, []);
-
-  const getChampionName = (championId: number) => {
-    if (!championsData) return "";
-
-    for (const championName in championsData) {
-      if (championsData[championName].key === championId.toString()) {
-        return championName;
-      }
-    }
-
-    return "";
-  };
-
-  //Getting the picture of the champion
-
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const handleSearch = async (summonerName: string, region: string) => {
     try {
       const response = await fetch(
-        `/api/summoner?summonerName=${summonerName}`
+        `/api/api?summonerName=${encodeURIComponent(
+          summonerName
+        )}&region=${encodeURIComponent(region)}`
       );
-      const data = await response.json();
-      setSummonerName(data.summonerName);
-      setSummonerLevel(data.summonerLevel);
-      setSummonerId(data.summonerId);
-      setSummonerRank(data.summonerRank);
-      setSummonerTier(data.summonerTier);
-      setSummonerLp(data.summonerLp);
-      setSummonerWins(data.summonerWins);
-      setSummonerLosses(data.summonerLosses);
-      setSummonerWinrate(data.summonerWinrate);
-      setSummonerIconUrl(data.summonerIconUrl);
-      setChampionMasteries(data.championMasteries);
+
+      if (response.ok) {
+        const data = await response.json();
+        setLevel(data.summonerData.summonerLevel);
+        setPuuid(data.puuid);
+        fetchMatchHistory(data.puuid, region);
+      } else {
+        console.error("Failed to fetch summoner data:", response.statusText);
+      }
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching summoner data:", error);
+    }
+  };
+
+  const fetchMatchHistory = async (puuid: string, region: string) => {
+    try {
+      const matchRegion = getMatchRegion(region);
+      const response = await fetch(
+        `/api/matchhistory?puuid=${encodeURIComponent(
+          puuid
+        )}&region=${encodeURIComponent(matchRegion)}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setMatchIds(data);
+      } else {
+        console.error("Failed to fetch match history:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching match history:", error);
     }
   };
 
   return (
     <div>
-      <div className="search-container">
-        <form onSubmit={handleFormSubmit}>
-          <Grid.Container justify="center" alignItems="center">
-            <Grid xs={12} sm={4}>
-              <Input
-                placeholder="Summoner Name"
-                value={summonerName}
-                onChange={(e) => setSummonerName(e.target.value)}
-                size="lg"
-              />
-            </Grid>
-            <Grid xs={12} sm={2}>
-              <Spacer x={1} />
-              <Button type="submit" size="lg" aria-label="Search">
-                Search
-              </Button>
-            </Grid>
-          </Grid.Container>
-        </form>
-      </div>
-      {summonerId && (
-        <div className="summoner-details">
-          <Card>
-            <Image
-              src={summonerIconUrl}
-              alt={`${summonerName}'s profile icon`}
-            />
-            <div className="summoner-info">
-              <Text h4>Summoner name: {summonerName}</Text>
-              <Text>Summoner level: {summonerLevel}</Text>
-              <Text>Summoner ID: {summonerId}</Text>
-              <Text>Summoner rank: {summonerRank}</Text>
-              <Text>Summoner tier: {summonerTier}</Text>
-              <Text>Summoner LP: {summonerLp}</Text>
-              <Text>Summoner wins: {summonerWins}</Text>
-              <Text>Summoner losses: {summonerLosses}</Text>
-              <Text>Summoner winrate: {summonerWinrate}</Text>
-            </div>
-          </Card>
+      <h1>Project1</h1>
+      <SearchBar onSearch={handleSearch} />
+      {level && <p>Summoner Level: {level}</p>}
+      {puuid && <p>PUUID: {puuid}</p>}
+      {matchIds && (
+        <div>
+          <h2>Last 10 Match IDs:</h2>
+          <ul>
+            {matchIds?.map((match: { matchId: string; matchDetails: any }) => (
+              <li key={match.matchId}>{match.matchId}</li>
+            ))}
+          </ul>
         </div>
       )}
-      {championMasteries.length > 0 && (
-        <div className="champion-masteries">
-          <Text h4>Most Played Champions</Text>
-          <Table
-            aria-label="Most played champions table"
-            css={{
-              height: "auto",
-              minWidth: "100%",
-            }}
-          >
-            <Table.Header>
-              <Table.Column>Champion</Table.Column>
-              <Table.Column>Name</Table.Column>
-              <Table.Column>Mastery Points</Table.Column>
-            </Table.Header>
-            <Table.Body>
-              {championMasteries.slice(0, 5).map((mastery, index) => {
-                const championInfo = getChampionInfo(mastery.championId);
-                return (
-                  <Table.Row key={index}>
-                    <Table.Cell
-                      css={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        paddingLeft: 0,
-                        paddingRight: 0,
-                      }}
-                    >
-                      <Image
-                        src={championInfo.imageUrl}
-                        alt={`${championInfo.name} profile icon`}
-                        width={40}
-                        height={40}
-                      />
-                    </Table.Cell>
-                    <Table.Cell>
-                      {index + 1}. {championInfo.name}
-                    </Table.Cell>
-                    <Table.Cell>
-                      {mastery.championPoints.toLocaleString()} Points
-                    </Table.Cell>
-                  </Table.Row>
-                );
-              })}
-            </Table.Body>
-          </Table>
-        </div>
-      )}
-      <style jsx>{`
-        .search-container {
-          padding: 2rem 0;
-        }
-        @media (max-width: 767px) {
-          .search-container {
-            padding: 1rem 0;
-          }
-        }
-        .summoner-details {
-          margin: 2rem auto;
-          max-width: 600px;
-        }
-        .summoner-info {
-          padding: 1rem;
-        }
-        .champion-masteries {
-          margin: 2rem auto;
-          max-width: 600px;
-        }
-        .champion-masteries-info {
-          padding: 1rem;
-        }
-        .champion-mastery {
-          margin-bottom: 0.5rem;
-        }
-      `}</style>
     </div>
   );
-}
+};
+
+export default Project1;
